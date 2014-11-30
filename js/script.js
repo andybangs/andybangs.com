cf1ce2ea5a39e095d4db9979bde676f66a434342
Andy Bangs <andrew.yuta@gmail.com>
2014-11-30 19:11:09 +0900

    Delete unused assets

--- js/script.js
+++ js/script.js
@@ -2,39 +2,8 @@
 ---Change Background---
 **********************/
 
-$(document).ready(function() {	
-	$('.logo').on('click', function() {
-			$('body').toggleClass('bg2');
+$(document).ready(function () {
+	$('.logo').on('click', function () {
+		$('body').toggleClass('bg2');
 	});
 });
-
-// For 3 or more background images
-
-// var imageIndex = 0;
-// var imagesArray = [
-//     "images/wbg_arashiyama.jpg",
-//     "images/wbg_train.jpg",
-//     "images/wbg_hozugawa.jpg"
-// ];
-
-// function changeBackground(){
-//     var index = imageIndex++ % imagesArray.length;
-//     $("body").css({"background":"linear-gradient(rgba(62,62,62,.5), rgba(62,62,62,.5)), url('"+ imagesArray[index] +"') center center fixed no-repeat",
-// 				   				 "background-size":"cover",
-// 				   				 "width":"100%",
-// 				   				 "height":"100%"});  
-// }
-
-// $(document).ready(function() {	
-// 	$('.logo').on('click', function() {
-// 			changeBackground();
-// 	});
-// });
-
-// $(document).ready(function() {
-//   setInterval(changeBackground, 8000);
-// });
-
-
-
-
